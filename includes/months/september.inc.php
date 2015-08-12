b5ac1d7fa2fe5a8c56092ddfb19a7c79ca3c5c81
Stacie Farmer <farmerwebdesign@gmail.com>
2015-08-12 14:40:21 -0600

    Create September with 3 events to start

--- includes/months/september.inc.php
+++ includes/months/september.inc.php
@@ -1,14 +1,14 @@
 
 <section id="month_header">
-	<a href="index.php?month=july">
+	<a href="index.php?month=august">
 		<i class="fa fa-arrow-left"></i>
 	</a>
-	<h1 class="month_name">August</h1>
-	<a href="index.php?month=september">
+	<h1 class="month_name">September</h1>
+	<a href="javascript:void(0);" class="inactive_link">
 		<i class="fa fa-arrow-right"></i>
 	</a>
 </section>
-<input type='hidden' id='start_date' value='6' />
+<input type='hidden' id='start_date' value='2' />
 <table class="table table-bordered">
 	<thead>
 		<tr>
