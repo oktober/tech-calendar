2a79f46efce4246396c3f87258a39893d70477e2
Stacie Farmer <farmerwebdesign@gmail.com>
2015-09-03 16:24:01 -0600

    Update Sep and add Oct events

--- includes/months/october.inc.php
+++ includes/months/october.inc.php
@@ -1,14 +1,14 @@
 
 <section id="month_header">
-	<a href="index.php?month=august">
+	<a href="index.php?month=september">
 		<i class="fa fa-arrow-left"></i>
 	</a>
-	<h1 class="month_name">September</h1>
-	<a href="index.php?month=october">
+	<h1 class="month_name">October</h1>
+	<a href="javascript:void(0);" class="inactive_link">
 		<i class="fa fa-arrow-right"></i>
 	</a>
 </section>
-<input type='hidden' id='start_date' value='2' />
+<input type='hidden' id='start_date' value='4' />
 <table class="table table-bordered">
 	<thead>
 		<tr>
