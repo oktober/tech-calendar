d7db890fb156e5b472161b419d43e880b063fb9f
Stacie Farmer <farmerwebdesign@gmail.com>
2015-09-24 11:01:01 -0600

    Move js to separate file, add hide show for categories, update some tyles

--- index.php
+++ index.php
@@ -17,10 +17,38 @@
 
 	<section class="container">
 		<section class="row">
-			<div class="span1" style="border: 1px solid red;">
+			<div class="col-md-12" style="border: 1px solid red;">
+				<span class="categories dotnet"><a>.NET</a></span>
+				<span>Agile</span>
+				<span>C++</span>
+				<span>Data Science</span>
+				<span>Databases</span>
+<!--Development-->
+				<span class="categories devops"><a>DevOps</a></span>
+<!--Drupal
+Elixir
+Hackerspace
+Java
+Javascript
+Management
+Mobile Development-->
+				<span class="categories os"><a>Open Source</a></span>
+<!--PHP
+Python
+Ruby
+Salesforce
+Scala
+Security
+Software as a Service
+SQL
+Tech
+Test Automation/QA-->
 			</div>
+		</section>
+		
+		<section class="row">
 
-			<div class="span11">
+			<div class="col-md-12">
 			<?php
 				date_default_timezone_set('America/Denver');
 				$valid_months = array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december');
@@ -48,65 +76,6 @@
 
     <script src="includes/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <script>
-    $(function () {
-	  	$('[data-toggle="tooltip"]').tooltip();
-
-	  //use AJAX function to find JSON file for current month
-	  //read through the data and load into HTML table
-	  	var month_data_file = "includes/months/" + $('.month_name').text().toLowerCase() + "_events.json?ver=1.1";
-	  	$.getJSON( month_data_file, function( data ) {
-
-			var items = "";
-			var start_day = $('#start_date').val(); //Sunday = 0, Monday = 1, Tuesday = 2, etc
-
-		  	items += '<tr>';
-		  	var day_counter = 0;
-		  	while(day_counter < start_day){
-		  		//add empty cells to account for the start date of the month
-		  		items += '<td></td>';
-		  		day_counter++;
-		  	}
-
-		  	$.each( data, function( day, time_object ) {
-		  		//loop through each time in the day
-		  		items += '<td><section>';
-		  		items += '<section class="date">'+day+'</section>';
-
-		  		$.each( time_object, function( time, event_object ) {
-		  			//loop through each event at that time
-		  			items += '<section class="time">'+time+'</section>';
-
-				  	for (var i in event_object) {
-				  		//console.log(event_object[i].group); //put this in a new section under the time
-				  		items += '<section class="event_name';
-				  		if(i === 0){ //only add this formatting (class) for the first event at that time
-				  			items += ' first';
-				  		}
-				  		items += ' '+event_object[i].category+'"><a href="'+event_object[i].link+'" target="_blank" data-toggle="tooltip" title="'+event_object[i].group+'">'+event_object[i].event_name+'</a></section>';
-				  	}
-		  		});
-		  		items += '</section></td>';
-
-				day_counter++;
-		  		if(day_counter === 7) { //if it's the end of the week, start a new row
-		  			items += '</tr><tr>';
-		  			day_counter = 0;
-		  		}
-		  	});
-	  		if(day_counter < 7) { //if there are days left in the week, make them empty
-	  			while(day_counter < 7){
-			  		items += '<td></td>';
-			  		day_counter++;
-	  			}
-	  			items += '</tr><tr>';
-	  			day_counter = 0;
-	  		}
-		  	items += '</tr>';
-		 
-		  $("tbody").html(items);
-		});
-	})
-    </script>
+    <script src="includes/scripts.js"></script>
 </body>
 </html>
