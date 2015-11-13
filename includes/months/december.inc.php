cd46066aa058e2c24f191270e800b57d3b319039
Stacie Farmer <farmerwebdesign@gmail.com>
2015-11-13 10:00:52 -0700

    Update Nov events and add Dec calendar

--- includes/months/december.inc.php
+++ includes/months/december.inc.php
@@ -1,14 +1,14 @@
 
 <section id="month_header">
-	<a href="index.php?month=october">
+	<a href="index.php?month=november">
 		<i class="fa fa-arrow-left"></i>
 	</a>
-	<h1 class="month_name">November</h1>
-	<a href="index.php?month=december">
+	<h1 class="month_name">December</h1>
+	<a href="javascript:void(0);" class="inactive_link">
 		<i class="fa fa-arrow-right"></i>
 	</a>
 </section>
-<input type='hidden' id='start_date' value='0' />
+<input type='hidden' id='start_date' value='2' />
 <table class="table table-bordered">
 	<thead>
 		<tr>
