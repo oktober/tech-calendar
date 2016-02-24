959b59eb65ed719a97b23b973952d28fc8c4c0e7
Stacie Farmer <farmerwebdesign@gmail.com>
2016-02-24 09:20:35 -0700

    Create new files for April calendar

---
+++ includes/months/april.inc.php
@@ -0,0 +1,40 @@
+
+<section id="month_header">
+	<a href="index.php?month=march">
+		<i class="fa fa-arrow-left"></i>
+	</a>
+	<h1 class="month_name">April</h1>
+	<a href="javascript:void(0);" class="inactive_link">
+		<i class="fa fa-arrow-right"></i>
+	</a>
+</section>
+<input type='hidden' id='start_date' value='5' />
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th>
+				Sunday
+			</th>
+			<th>
+				Monday
+			</th>
+			<th>
+				Tuesday
+			</th>
+			<th>
+				Wednesday
+			</th>
+			<th>
+				Thursday
+			</th>
+			<th>
+				Friday
+			</th>
+			<th>
+				Saturday
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+	</tbody>
+</table>
